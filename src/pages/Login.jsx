a8d64e9ffecd1ae2a2ccdab6608e69009f3b10c9
DAP2506 <dapanchal2506@gmail.com>
2022-02-09 17:42:07 +0530

    Login SignUp UI added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,104 @@
 import React from 'react'
 import './CSS/Login.css'
+import logo from "./images/img-01.png"
+import { MdArrowRightAlt, MdEmail } from "react-icons/md";
+import { BsLockFill } from "react-icons/bs";
+import { Link } from 'react-router-dom';
+import Lottie from 'react-lottie';
+import animationData from '../utils/lotties/animi.json'
+
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+
+    <div class="limiter">
+
+      <div class="container-login100">
+        <div class="wrap-login100">
+          <div class="login100-pic js-tilt" >
+
+            
+            {<Lottie
+              options={defaultOptions}
+              height={400}
+              width={400}
+            />
+            
+
+            || <img src={logo} alt="IMG" ></img>}
+
+          </div>
+
+          <form class="login100-form validate-form">
+            <span class="login100-form-title">
+              ACCESS DENIED WEBSITE
+            </span>
+
+            <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+              <input class="input100" type="text" name="email" placeholder="Email" />
+              <span class="focus-input100"></span>
+              <span class="symbol-input100">
+                <MdEmail />
+              </span>
+            </div>
+
+            <div class="wrap-input100 validate-input" data-validate="Password is required">
+              <input class="input100" type="password" name="pass" placeholder="Password" />
+              <span class="focus-input100"></span>
+              <span class="symbol-input100">
+                <BsLockFill />
+              </span>
+            </div>
+
+            <div class="container-login100-form-btn">
+              <button class="login100-form-btn">
+                Login
+              </button>
+            </div>
+
+            <br></br>
+            <br></br>
+
+            <div class="text-center ">
+              <span class="txt1">
+                Forgot
+              </span>
+              <a class="txt2" href="#">
+                Username / Password?
+              </a>
+            </div>
+
+            <div class="">
+              <a class="txt2" href="#">
+                <Link to='/register' >
+                  Create your Account <MdArrowRightAlt />
+                </Link>
+
+              </a>
+            </div>
+          </form>
+        </div>
+
+
+      </div>
+
+
+    </div>
+
+
+
+
   )
 }
 
-export default Login
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,106 @@
 import React from 'react'
 import './CSS/Register.css'
+import logo from "./images/img-01.png"
+import { MdArrowRightAlt, MdEmail } from "react-icons/md";
+import { BsLockFill } from "react-icons/bs";
+import { Link } from 'react-router-dom';
+import Lottie from 'react-lottie';
+import animationData from '../utils/lotties/animi.json'
+
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
+
 
 const Register = () => {
   return (
-    <div>Register</div>
+
+    <div class="limiter">
+
+      <div class="container-login100">
+        <div class="wrap-login100">
+          <div class="login100-pic js-tilt" >
+
+
+            {
+              <Lottie
+                options={defaultOptions}
+                height={400}
+                width={400}
+              />
+              ||
+              <img src={logo} alt="IMG" ></img>
+            }
+
+          </div>
+
+          <form class="login100-form validate-form">
+            <span class="login100-form-title">
+              ACCESS DENIED WEBSITE
+            </span>
+
+            <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+              <input class="input100" type="text" name="email" placeholder="Email" />
+              <span class="focus-input100"></span>
+              <span class="symbol-input100">
+                <MdEmail />
+              </span>
+            </div>
+
+            <div class="wrap-input100 validate-input" data-validate="Password is required">
+              <input class="input100" type="password" name="password" placeholder="Password" />
+              <span class="focus-input100"></span>
+              <span class="symbol-input100">
+                <BsLockFill />
+              </span>
+            </div>
+
+
+            <div class="wrap-input100 validate-input" data-validate="Password is required">
+              <input class="input100" type="password" name="confirmpassword" placeholder="Confirm Password" />
+              <span class="focus-input100"></span>
+              <span class="symbol-input100">
+                <BsLockFill />
+              </span>
+            </div>
+
+            <div class="container-login100-form-btn">
+              <button class="login100-form-btn">
+                Sign Up
+              </button>
+            </div>
+
+            <br></br>
+            <br></br>
+
+
+            <div class="">
+              <a class="txt2" href="#">
+                <Link to='/login' >
+                  Already have an account <MdArrowRightAlt />
+                </Link>
+
+              </a>
+            </div>
+          </form>
+        </div>
+
+
+      </div>
+
+
+    </div>
+
+
+
+
   )
 }
 
-export default Register
+export default Register;
