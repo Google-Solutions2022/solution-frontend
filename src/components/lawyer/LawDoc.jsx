d69fe2d411de5e5aeace056eaae08177671804a1
Mohit Agrawal <mohit27agrawal@gmail.com>
2022-02-19 12:34:11 +0530

    Small Changes

--- src/components/lawyer/LawDoc.jsx
+++ src/components/lawyer/LawDoc.jsx
@@ -1,15 +1,60 @@
-import { faDownload, faFileLines } from '@fortawesome/free-solid-svg-icons'
+import { faDownload, faFileArrowUp, faFileLines } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useState } from 'react'
 import documentData from '../../Json_files/law_docs.js'
 import './CSS/LawDoc.css'
 const LawDoc = () => {
+  const [up, setUp] = useState("closed");
+  function open() {
+    const uploadC = document.getElementById("upload_container");
+    const uploadB = document.getElementById("upload_body");
+    if(up === "closed") {
+      setUp("opened");
+      uploadC.classList.remove("open");
+      uploadB.classList.add("active");
+    }
+  }
+  function close() {
+    const uploadC = document.getElementById("upload_container");
+    const uploadB = document.getElementById("upload_body");
+    if(up === "opened") {
+      setUp("opened");
+      uploadC.classList.add("open");
+      uploadB.classList.remove("active");
+    }
+  }
   return (
     <div>
       <div className="law_docs">
-      <div className="upload_docs">
-
-      </div>
+        <div className="upload_docs">
+          <div className="upload_docs_head open" id="upload_conatiner">
+            <h3 className="upload_head active" onClick={()=>open()}>Upload your document Here</h3>
+            <div className="doc_upload" id="upload_body">
+              <form action="../">
+                <div className="form-floating mb-3">
+                  <input type="text" className="form-control updoc_name" id="floatingInput" placeholder="Document Name" />
+                  {/* <label for="floatingInput">Document Name</label> */}
+                </div>
+                <div className="form-floating">
+                  <input type="text" className="form-control updoc_des" id="floatingInput" placeholder="Documnet Description" />
+                  {/* <label for="floatingPassword">Password</label> */}
+                </div>
+                <div className="form_down">
+                  <div className="upload_form">
+                    <input type="file" id="uploadFile" hidden />
+                    <label htmlFor="uploadFile">
+                      <FontAwesomeIcon icon={faFileArrowUp} />
+                    </label>
+                  </div>
+                  <div className="upload_act">
+                    <button className="btn cancel_btn" onClick={close}>Cancel</button>
+                    <button className="btn upload_btn">Upload</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
         <div className="docs_available">
           <h3>Uploaded / Available Documents</h3>
           <div className="available_doc">
