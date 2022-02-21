d12bad2c5e1fcfe3849415d3da0207e422d02fd7
Dhvani <92153529+dhvani5@users.noreply.github.com>
2022-02-21 12:59:10 +0800

    updated homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -320,19 +320,19 @@ const Home = () => {
     <ul class="hash-list cols-3 cols-1-xs pad-30-all align-center text-sm">
         <li>
           <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="wpx-100 img-round mgb-20" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]"/>
-          <p class="fs-110 font-cond-l" contenteditable="false">" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. "</p>
-          <h5 class="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">Martha Stewart</h5>
-          <small class="font-cond case-u lts-sm fs-80 fg-text-l" contenteditable="false">Business Woman - New York</small>
+          <p class="fs-110 font-cond-l" contenteditable="false">" Got my documents gathered and prepared at the comfort of my home. Great platform, brilliant online support, proved helpful indeed! "</p>
+          <h5 class="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">John Stewart</h5>
+          <small class="font-cond case-u lts-sm fs-80 fg-text-l" contenteditable="false">Businessman - New York</small>
         </li>
         <li>
           <img src="https://bootdey.com/img/Content/avatar/avatar4.png" class="wpx-100 img-round mgb-20" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]"/>
-          <p class="fs-110 font-cond-l" contenteditable="false">" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. "</p>
+          <p class="fs-110 font-cond-l" contenteditable="false">" Consulted a doctor on this website, and I must say the online consultancy proved exceptionally effective, much beyond expectation. "</p>
           <h5 class="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">Ariana Menage</h5>
           <small class="font-cond case-u lts-sm fs-80 fg-text-l" contenteditable="false">Recording Artist - Los Angeles</small>
         </li>
         <li>
           <img src="https://bootdey.com/img/Content/avatar/avatar5.png" class="wpx-100 img-round mgb-20" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]"/>
-          <p class="fs-110 font-cond-l" contenteditable="false">" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. "</p>
+          <p class="fs-110 font-cond-l" contenteditable="false">" I got all my paperwork done at ease. <br/> I would highly recommend this website. <br/> It is a wonderful initiative!"<br/><br/></p>
           <h5 class="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">Sean Carter</h5>
           <small class="font-cond case-u lts-sm fs-80 fg-text-l" contenteditable="false">Fund Manager - Chicago</small>
         </li>
