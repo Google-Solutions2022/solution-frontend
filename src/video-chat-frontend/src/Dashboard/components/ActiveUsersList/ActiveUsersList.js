a9f61e7222b52a5f245ba962d8511543cff279f3
ui20cs38 <harshmavani06@gmail.com>
2022-03-31 11:00:34 +0530

    video chat added

---
+++ src/video-chat-frontend/src/Dashboard/components/ActiveUsersList/ActiveUsersList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ActiveUsersListItem from './ActiveUsersListItem';
+import { connect } from 'react-redux';
+
+import './ActiveUsersList.css';
+
+const ActiveUsersList = ({ activeUsers, callState }) => {
+  return (
+    <div className='active_user_list_container'>
+      <h1 style={{textAlign: "center"}}>  List of Doctors</h1>
+      {activeUsers.map((activeUser) =>
+        <ActiveUsersListItem
+          key={activeUser.socketId}
+          activeUser={activeUser}
+          callState={callState}
+        />)}
+    </div>
+  );
+};
+
+const mapStateToProps = ({ dashboard, call }) => ({
+  ...dashboard,
+  ...call
+});
+
+export default connect(mapStateToProps)(ActiveUsersList);
